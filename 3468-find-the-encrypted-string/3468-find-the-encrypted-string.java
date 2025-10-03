47c4ff52cf03f6e4335f4e6f2dca8132486e73a9
logywaran <150234568+logywaran@users.noreply.github.com>
2025-10-03 09:15:35 +0530

    Time: 7 ms (11.66%), Space: 43.8 MB (12.44%) - LeetHub

--- 3468-find-the-encrypted-string/3468-find-the-encrypted-string.java
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
-        char[] sarr = s.toCharArray();
+        
         String str ="" ;
         
         
-        for(int i=0;i<sarr.length;i++){
-            int m = (i+k)%sarr.length;
-            str+= sarr[m];
+        for(int i=0;i<s.length();i++){
+            int m = (i+k)%s.length();
+            str+= s.charAt(m);
 
         }
        
